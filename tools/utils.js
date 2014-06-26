f356ea312bf0eab8896cc0c3b5f63883178c8230
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-06-26 16:23:02 +0300

    crude fix for error handling bugs

--- tools/utils.js
+++ tools/utils.js
@@ -67,7 +67,13 @@ exports.handleHttpResults = function handleHttpResults(req,res){
         //console.log("DEBUG: handleResultsFunc: ", req.path)
         //console.log("RESULT", JSON.stringify((result)))
         //throw new Error("handleResults: error retrieving results: "+err);
-
+	
+	err = result ? err : (err || {});
+	
+	if (err){
+	    err.code = err.code || 500;
+	}
+	
         if (err) {
             return res.send(err.code,err); //TODO: this will break asaf code???
             console.error("ERROR: handling error", err, err.stack);
