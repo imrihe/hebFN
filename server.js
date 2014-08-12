c7f5629107ff005a0fc0cc4dafd3971b3d7a7368
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-12 16:11:47 +0300

    Create basic nodejs+express webserver

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/partials/:name', function (req, res) {
+    var name = req.params.name;
+    res.render('partials/' + name);
+});
+
+app.listen(3000);
