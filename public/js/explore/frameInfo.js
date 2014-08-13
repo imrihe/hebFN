906cc4fa7a5c1725914db075ac694e2525d2e78f
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-13 16:26:24 +0300

    Fetch the correct frame (or the first one as default)

--- public/js/explore/frameInfo.js
+++ public/js/explore/frameInfo.js
@@ -2,9 +2,9 @@
     angular.module('fnExplore').
 	directive('frameInfo', frameInfo);
 
-    frameInfo.$inject = ['listFrames'];
+    frameInfo.$inject = ['$routeParams', 'listFrames'];
 
-    function frameInfo(listFrames) {
+    function frameInfo($routeParams, listFrames) {
 	return {
 	    templateUrl: 'partials/explore/frame-info.html',
 	    restrict: 'E',
@@ -12,6 +12,23 @@
 	    controllerAs: 'exploreInfo'
 	};
 
-	function infoCtrl(){};
+	function infoCtrl(){
+	    var infoCtrl = this;
+
+	    this.info = {};
+
+	    activate();
+
+	    /// initialization ///
+	    function activate(){
+		listFrames.then(function(response){
+		    var data = response.data;
+		    var name = ($routeParams.frame || data[0].frame['@name']).toLowerCase();
+
+		    infoCtrl.info = data.filter(function(x){ return x.frame['@name'].toLowerCase() === name; })[0].frame;
+		});
+	    }
+	};
+
     };
 })();
