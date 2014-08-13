3fba0a6321b576254c944d4d8d8c339753746a31
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-13 15:56:07 +0300

    fix frame name search

--- public/js/explore.js
+++ public/js/explore.js
@@ -22,40 +22,50 @@
 	    var searchCtrl = this;
 
 	    var pageSize = 20;
-	    var numPages = 0;
 	    var frames = [];
+	    var fullSearchResults;
 
 	    activate();
 
 	    this.currentPage = 0;
+	    this.numPages = 0;
 	    this.searchResults = [];
 
+	    this.query = '';
+
 	    this.isFirstPage = function() {
-		return !numPages || searchCtrl.currentPage === 1;
+		return !searchCtrl.numPages || searchCtrl.currentPage === 1;
 	    };
 
 	    this.isLastPage = function() {
-		return !numPages || searchCtrl.currentPage === numPages;
+		return !searchCtrl.numPages || searchCtrl.currentPage === searchCtrl.numPages;
 	    };
 
 	    this.changePage = function(page) {
-		if (page > numPages) {
-		    page = numPages;
+		if (page > searchCtrl.numPages) {
+		    page = searchCtrl.numPages;
 		} else if (page < 1) {
 		    page = 1;
 		}
 
-		searchCtrl.searchResults = frames.slice((page-1)*pageSize, page*pageSize);
+		searchCtrl.searchResults = fullSearchResults.slice((page-1)*pageSize, page*pageSize);
 
 		searchCtrl.currentPage = page;
+	    }
+
+	    this.filterFrames = function(query) {
+		query = query.toLowerCase();
+		fullSearchResults = frames.filter(function(x){return x.toLowerCase().indexOf(query) >= 0;});
+		searchCtrl.numPages = Math.ceil(fullSearchResults.length / pageSize);
+		searchCtrl.changePage(1);
 	    }
 
 	    /// initialization ///
 	    function activate(){
 		listFrames.then(function(response){
 		    var data = response.data;
-		    frames = data.map(function(x) { return x.frame['@name']; });
-		    numPages = frames.length / pageSize;
+		    fullSearchResults = frames = data.map(function(x) { return x.frame['@name']; });
+		    searchCtrl.numPages = Math.ceil(frames.length / pageSize);
 
 		    searchCtrl.changePage(1);
 		});
