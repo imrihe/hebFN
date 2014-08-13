21be051433220a4b06a0f0a06e153fc5fd08811e
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-13 10:13:44 +0300

    declare directives for custom elements

--- public/js/explore.js
+++ public/js/explore.js
@@ -1,7 +1,24 @@
 (function() {
     angular.module('fnExplore', []);
 
-    angular.module('fnExplore').controller('explore', explore);
+    angular.module('fnExplore').
+	controller('explore', explore).
+	directive('frameSearch', frameSearch).
+	directive('frameInfo', frameInfo);
 
     function explore() {};
+
+    function frameSearch() {
+	return {
+	    templateUrl: "'partials/explore/frame-search.html'",
+	    restrict: 'E'
+	};
+    };
+
+    function frameInfo() {
+	return {
+	    templateUrl: "'partials/explore/frame-info.html'",
+	    restrict: 'E'
+	};
+    };
 })();
