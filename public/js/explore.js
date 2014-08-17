b88bed93f31f8f35b5abb7af0b26bce448421847
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-17 11:38:54 +0300

    drop unused dependency

--- public/js/explore.js
+++ public/js/explore.js
@@ -1,5 +1,5 @@
 (function() {
-    angular.module('fnExplore', ['ngSanitize', 'fnServices', 'hebFN.englishFrame']);
+    angular.module('fnExplore', ['fnServices', 'hebFN.englishFrame']);
 
     angular.module('fnExplore').
 	controller('exploreMain', explore);
