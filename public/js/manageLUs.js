d38e3cf5e9f1bf18ee278288ca3f4974927c5302
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-18 13:09:49 +0300

    Finish up the manage lus ui

--- public/js/manageLUs.js
+++ public/js/manageLUs.js
@@ -13,10 +13,39 @@
 	var frameName = $routeParams.frame;
 	var luName = $routeParams.lu;
 
-	this.info = {};
+	this.POSs = {
+            noun:"n",
+            verb:"v", 
+            adjective:"a",
+            adverb:"adv",
+            preposition:"prep",
+	    modal:"md"
+	};
+
+	this.statusValues = [
+	    "initial",
+	    "partial",
+	    "full"
+	];
+
+	this.frameInfo = {};
+	this.luInfo = {comments: [{cBy: 'avi', cDate: new Date(), content: 'alskjaskldjas kljasdkaslkjasd klasjkasd'}]};
+
+	this.addSemType = function () {
+	    if (self.semType) {
+		self.luInfo.semType.push(self.semType);
+	    }
+	};
+
+	this.removeSemType = function (idx) {
+	    self.luInfo.semType.splice(idx,1);
+	};
+
+	this.saveLU = function () {
+	};
 
 	frameDataManager.frameData(frameName).then(function(response){
-	    self.info = response.data;
+	    self.frameInfo = response.data;
 	});
     }
 })();
