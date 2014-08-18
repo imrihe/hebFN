041b153b2b1ccf32594a7c7240d7a74c44413ca4
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-18 16:06:54 +0300

    Connect the form elements to the relevant bits of data from the server

--- public/js/manageLUs.js
+++ public/js/manageLUs.js
@@ -6,13 +6,13 @@
     ]).
 	controller('manageLU', manageLU);
 
-    manageLU.$injector = ['$routeParams',  'frameDataManager'];
+    manageLU.$injector = ['$routeParams', 'frameDataManager', 'luDataManager'];
 
-    function manageLU ($routeParams, frameDataManager) {
+    function manageLU ($routeParams, frameDataManager, luDataManager) {
 	var self = this;
-	
+	var luName = $routeParams.lu;
+
 	this.frameName = $routeParams.frame;
-	this.luName = $routeParams.lu;
 
 	this.POSs = {
             noun:"n",
@@ -30,7 +30,24 @@
 	];
 
 	this.frameInfo = {};
-	this.luInfo = {comments: [{cBy: 'avi', cDate: new Date(), content: 'alskjaskldjas kljasdkaslkjasd klasjkasd'}]};
+	this.luInfo = {};
+
+	this.luName = function (name) {
+	    if (!name) {
+		return getLUprop('@name').split('.')[0];
+	    } else {
+		self.luInfo['@name'] = name + '.' + self.luPOS();
+	    }
+	};
+
+	this.luPOS = function (pos) {
+	    if (!pos) {
+		return getLUprop('@POS').toLowerCase();
+	    } else {
+		self.luInfo['@POS'] = pos.toUpperCase();
+		self.luInfo['@name'] = self.luName() + '.' + pos;
+	    }
+	};
 
 	this.addSemType = function () {
 	    if (self.semType) {
@@ -44,5 +61,29 @@
 
 	this.saveLU = function () {
 	};
+
+	function getLUprop () {
+	    var propPath = Array.prototype.slice.call(arguments);
+	    var result = self.luInfo;
+
+	    propPath.every(function (e, i, a) {
+		if (result[e]) {
+		    result = result[e];
+		} else {
+		    result = '';
+		    return false;
+		}
+	    });
+
+	    return result;
+	};
+
+	frameDataManager.frameData(this.frameName).then(function (result) {
+	    self.frameInfo = result.data;
+	});
+
+	luDataManager.luData(this.frameName, luName).then(function (result) {
+	    self.luInfo = result.data;
+	});
     }
 })();
