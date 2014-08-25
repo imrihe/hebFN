ce8535eeb25a35d3ace71b922a63de94a23a296d
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-25 14:54:19 +0300

    drop route to view that was changed to a directive

--- public/js/app.js
+++ public/js/app.js
@@ -25,12 +25,7 @@
 		templateUrl: 'partials/explore.html',
 		controller: 'exploreMain',
 		controllerAs: 'explore'
-	    }).
-	    when('/:frame/manage', {
-		templateUrl: 'partials/manage-frame.html',
-		controller: 'manageFrame',
-		controllerAs: 'manageFrame'
-	    }).when('/:frame/lu/:lu?', {
+	    })..when('/:frame/lu/:lu?', {
 		templateUrl: 'partials/manage-lu.html',
 		controller: 'manageLU',
 		controllerAs: 'manageLU'
