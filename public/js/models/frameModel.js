e982df5093b6baca0358357c6a3bbd1e30033014
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-25 14:26:03 +0300

    fix some problems with frameModel variable scope

--- public/js/models/frameModel.js
+++ public/js/models/frameModel.js
@@ -12,49 +12,50 @@
 	};
 
 	function FrameModel (frameName) {
+	    var frameDataURL = '//localhost:3003/heb/framedata',
+	        addCommentURL = '//localhost:3003/heb/addcomment';
+
 	    var self = this;
+
 	    this.name = frameName;
+	    this.addComment = addComment;
+
 	    frameData(this.name).then(function (result) {
 		angular.extend(self, result.data);
 	    });
+
+	    function frameData (name) {
+		var params = {framename: name};
+
+		return $http.get(frameDataURL, {
+		    params: params,
+		    cache: true,
+		    responseType: 'json'
+		});
+	    };
+
+	    function addComment (comment) {
+		var params = {
+		    type: 'frame',
+		    framename: this.name,
+		    comment: comment
+		};
+
+		$http.post(addCommentURL, {
+		    params: params,
+		    responseTyep: 'json'
+		}).then(function (result) {
+		    this.hebData.comments.push(res);
+		});
+	    };
 	};
 
-	frameModel.prototype.addComment = addComment;
-
-	var listFramesURL = '//localhost:3003/eng/framenames',
-            frameDataURL = '//localhost:3003/heb/framedata', 
-            addCommentURL = '//localhost:3003/heb/addcomment';
-
 	function listFrames () {
+	    var listFramesURL = '//localhost:3003/eng/framenames';
+
 	    return $http.get(listFramesURL, {
 		cache: true,
 		responseType: 'json'
-	    });
-	};
-
-	function frameData (name) {
-	    var frameDataURL = '//localhost:3003/heb/framedata';
-	    var params = {framename: name};
-
-	    return $http.get(frameDataURL, {
-		params: params,
-		cache: true,
-		responseType: 'json'
-	    });
-	};
-
-	function addComment (comment) {
-	    var params = {
-		type: 'frame',
-		framename: this.name,
-		comment: comment
-	    };
-
-	    $http.post(addCommentURL, {
-		params: params,
-		responseTyep: 'json'
-	    }).then(function (result) {
-		this.hebData.comments.push(res);
 	    });
 	};
     };
