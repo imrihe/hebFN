c35e8e77a36561d2eff65016f62783036229a10c
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-08-25 15:56:24 +0300

    Update to use the new luModel. This allows a massive cleanup

--- public/js/manageLUs.js
+++ public/js/manageLUs.js
@@ -1,15 +1,15 @@
 (function(){
     angular.module('hebFN.manageLUs', [
-	'fnServices',
+	'hebFN.models',
 	'hebFN.englishFrame',
 	'hebFN.commentsWidget'
     ]).
 	controller('manageLU', manageLU).
 	directive('validCompound', validCompound);
 
-    manageLU.$injector = ['$routeParams','$location', 'frameDataManager', 'luDataManager'];
+    manageLU.$injector = ['$routeParams','$location', 'frameDataService', 'luDataService'];
 
-    function manageLU ($routeParams, $location, frameDataManager, luDataManager) {
+    function manageLU ($routeParams, $location, frameDataService, luDataService) {
 	var self = this;
 	var luName = $routeParams.lu;
 
@@ -33,35 +33,11 @@
 	this.multiwordTypes = [{sep: '_', name: 'Contiguous compound'}, 
 				{sep: ' ', name: 'Non-contiguous compound'}];
 
-	this.frameInfo = {};
-	this.luInfo = {'@name': luName};
 	this.separator = this.multiwordTypes[0].sep;
-	
-	this.luName = getLUprop('@name').split('.')[0];
-
-	Object.defineProperties(this, {
-	    /*luName: {
-		get : function () {
-		    return getLUprop('@name').split('.')[0];
-		},
-		set: function (name) {
-		    self.luInfo['@name'] = (name || '') + '.' + self.luPOS;
-		}
-	    },*/
-	    luPOS: {
-		get: function () {
-		    return getLUprop('@name').split('.').pop() || self.luInfo['@POS'] || 'v';
-		},
-		set: function (pos) {
-		    self.luInfo['@POS'] = pos.toUpperCase();
-		    self.luInfo['@name'] = self.luName + '.' + pos;
-		}
-	    }
-	});
 
 	this.isCompound = function (val) {
 	    var result = false;
-	    val = val || self.luName || '';
+	    val = val || self.luInfo.name || '';
 	    
 	    self.multiwordTypes.forEach(function (x) {
 		result |= (val.indexOf(x.sep) > -1);
@@ -71,19 +47,19 @@
 	}
 
 	this.transformName = function () {
-	    if (!self.luName) return;
+	    if (!self.luInfo.name) return;
 
 	    self.multiwordTypes.forEach(function (x) {
 		var sepRegex = new RegExp(x.sep+'+', 'g');
-		self.luName = self.luName.replace(sepRegex, self.separator);
+		self.luInfo.name = self.luInfo.name.replace(sepRegex, self.separator);
 	    });
 
 	    var dashRegex = new RegExp(self.separator+'*-'+self.separator+'*', 'g');
-	    self.luName = self.luName.replace(dashRegex, self.separator+'-'+self.separator);
+	    self.luInfo.name = self.luInfo.name.replace(dashRegex, self.separator+'-'+self.separator);
 	}
 
 	this.isValidName = function () {
-	    return !!self.luName.length && self.isValidCompound();
+	    return !!self.luInfo.name.length && self.isValidCompound();
 	}
 
 	this.addSemType = function () {
@@ -96,51 +72,10 @@
 	    self.luInfo.semType.splice(idx,1);
 	};
 
-	this.saveLU = function () {
-	    luDataManager.saveLU(self.luInfo).then(function(res){
-		$location.path('/'+self.frameName+'/manage');
-	    });
-	};
-
-	this.addComment = function (comment) {
-	    var params = {
-		type: 'lu',
-		framename: self.frameName,
-		luname: self.luInfo['@name'],
-		comment: comment
-	    };
-
-	    luDataManager.addComment(params).then(function(res){
-		self.luInfo.comments.push(res);
-	    });
-	};
-
-	function getLUprop () {
-	    var propPath = Array.prototype.slice.call(arguments);
-	    var result = self.luInfo;
-
-	    propPath.every(function (e, i, a) {
-		if (result[e]) {
-		    result = result[e];
-		} else {
-		    result = '';
-		    return false;
-		}
-	    });
-
-	    return result;
-	};
-
-	frameDataManager.frameData(this.frameName).then(function (result) {
-	    self.frameInfo = result.data;
-	});
+	this.frameInfo = frameDataService.getFrame(this.frameName);
 
 	if (luName) {
-	    luDataManager.luData(this.frameName, luName).then(function (result) {
-		if (result.data) {
-		    self.luInfo = result.data;
-		}
-	    });
+	    this.luInfo = luDataService.getLU(this.frameName, luName);
 	}
     }
 
