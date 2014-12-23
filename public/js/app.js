9d938599dd79abda71ef9f18b36a48bfdf342563
Avi Hayoun <hayounav@cs.bgu.ac.il>
2014-12-23 22:35:09 +0200

    Annotation screen first iteration

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,14 @@
 (function(){
     angular.module('hebFN', [
-	'ngRoute',
-	'ngMessages',
-	'hebFN.models',
-	'hebFN.explore',
-	'hebFN.manageFrame',
-	'hebFN.manageLUs',
-	'hebFN.sentenceSearch',
-	'hebFN.constants'
+	    'ngRoute',
+	    'ngMessages',
+	    'hebFN.models',
+	    'hebFN.explore',
+	    'hebFN.manageFrame',
+	    'hebFN.manageLUs',
+	    'hebFN.sentenceSearch',
+        'hebFN.annotate',
+	    'hebFN.constants'
     ]).
 	config(config).
 	run(run).
@@ -19,88 +20,93 @@
     function config($routeProvider, $httpProvider) {
 	$routeProvider.
 	    when('/explore/:frame?', {
-		templateUrl: 'partials/explore.html',
-		controller: 'exploreMain',
-		controllerAs: 'explore'
+		    templateUrl: 'partials/explore.html',
+		    controller: 'exploreMain',
+		    controllerAs: 'explore'
 	    }).when('/edit/:frame/:lu?', {
-		templateUrl: 'partials/manage-lu.html',
-		controller: 'manageLU',
-		controllerAs: 'manageLU'
+		    templateUrl: 'partials/manage-lu.html',
+		    controller: 'manageLU',
+		    controllerAs: 'manageLU'
 	    }).when('/search/:frame/:lu', {
-		templateUrl: 'partials/sentence-search.html',
-		controller: 'sentenceSearch',
-		controllerAs: 'search'
+		    templateUrl: 'partials/sentence-search.html',
+		    controller: 'sentenceSearch',
+		    controllerAs: 'search'
 	    }).when('/sentences/:frame/:lu', {
-		templateUrl: 'partials/lu-sentences.html',
-		controller: 'luSentences',
-		controllerAs: 'sentences'
-	    }).otherwise('/explore');
+		    templateUrl: 'partials/lu-sentences.html',
+		    controller: 'luSentences',
+		    controllerAs: 'sentences'
+	    }).when('/annotate/:frame/:lu', {
+            templateUrl: 'partials/annotate.html',
+            controller: 'annotator',
+            controllerAs: 'annotator'
+        }).
+            otherwise('/explore');
 
-	$httpProvider.interceptors.
-	    push([
-		'$injector', 
-		function ($injector) {
-		    return $injector.get('AuthInterceptor')
-		}
-	    ]);
+	    $httpProvider.interceptors.
+	        push([
+		        '$injector', 
+		        function ($injector) {
+		            return $injector.get('AuthInterceptor')
+		        }
+	        ]);
     };
 
     run.$injector = ['$rootScope', '$timeout', 'Authenticator', 'serverConstants'];
 
     function run ($rootScope, $timeout, Authenticator, serverConstants){
-	Authenticator.ping().finally(function () {
-	    $rootScope.$on('event:loginRequired', function (e, next) {
-		// clear session
-		Authenticator.logout(true);
+	    Authenticator.ping().finally(function () {
+	        $rootScope.$on('event:loginRequired', function (e, next) {
+		        // clear session
+		        Authenticator.logout(true);
 
-		// show warning
-		$('#auth-error').removeClass('hide');
-		$timeout(function () {
-		    $('#auth-error').addClass('hide');
-		}, 3000);
+		        // show warning
+		        $('#auth-error').removeClass('hide');
+		        $timeout(function () {
+		            $('#auth-error').addClass('hide');
+		        }, 3000);
+	        });
 	    });
-	});
 
-	$rootScope.$on('event:loginError', function (e, next) {
-	    // show warning
-	    $('#login-error').removeClass('hide');
-	    $timeout(function () {
-		$('#login-error').addClass('hide');
-	    }, 3000);
-	});
+	    $rootScope.$on('event:loginError', function (e, next) {
+	        // show warning
+	        $('#login-error').removeClass('hide');
+	        $timeout(function () {
+		        $('#login-error').addClass('hide');
+	        }, 3000);
+	    });
     };
 
     interceptor.$injector = ['$rootScope', '$q'];
 
     function interceptor ($rootScope, $q) {
-	return {
-	    responseError: error
-	};
+	    return {
+	        responseError: error
+	    };
 	
-	function error(response) {
-	    var status = response.status;
+	    function error(response) {
+	        var status = response.status;
 	    
-	    if (response.config.url === '/login') {
-		$rootScope.$broadcast('event:loginError');
-	    } else if (status == 401) {
-		$rootScope.$broadcast('event:loginRequired');
-	    }
+	        if (response.config.url === '/login') {
+		        $rootScope.$broadcast('event:loginError');
+	        } else if (status == 401) {
+		        $rootScope.$broadcast('event:loginRequired');
+	        }
 
-	    return $q.reject(response);	    
-	}
+	        return $q.reject(response);	    
+	    }
     };
 
     ctrl.$injector = ['Authenticator'];
 
     function ctrl (Authenticator) {
-	var self = this;
+	    var self = this;
 	
-	this.login = function () {
-	   Authenticator.login(self.username, self.password);
-	};
+	    this.login = function () {
+	        Authenticator.login(self.username, self.password);
+	    };
 
-	this.logout = Authenticator.logout;
+	    this.logout = Authenticator.logout;
 
-	this.isLoggedIn = Authenticator.status;
+	    this.isLoggedIn = Authenticator.status;
     };
 })();
