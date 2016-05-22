92a8b459e9a22198e4429a93627a4670ec0cf3c1
avi <hayounav@cs.bgu.ac.il>
2016-05-22 09:25:53 +0300

    revert some bad changes

--- public/js/viewAnnotations.js
+++ public/js/viewAnnotations.js
@@ -18,53 +18,76 @@
         
         var raw_sentences = [];
         var old_annotations = [];
+        var selected = 0;
+        var selection = null;
         
         init();
+
+        self.select = function (index) {
+            selected = index;
+
+            self.activeSentence = raw_sentences[selected].content.fullSentence.words.slice(0);
+
+            updateAnnotations();
+        };
 
         self.isSpecialWord = function (w) {
             return w.match(/\*\w+\*/);
         };
 
-        self.reset = function () {
-            init();
-        };
+        function updateAnnotations () {
+            var sentences = [];
 
-        function removeTokenTag (sentence, tokens) {
-            if (!angular.isArray(tokens)) {
-                tokens = [tokens];
-            }
+            for (var i in self.annotations) {
+                var tokens = raw_sentences[i].content.fullSentence.words;
 
-            var ann = self.annotations[sentence];
+                var annotationSet = self.annotations[i].FE.label.
+                    concat(self.annotations[i].Target.label).
+                    sort(function (a, b) {
+                        if (a.tokens[0] < b.tokens[0] || b.itype) return -1;
+                        else if (a.tokens[0] > b.tokens[0] || a.itype) return 1;
+                        else return 0;
+                    });
 
-            tokens.forEach(function (t) {
-                for (var type in ann) {
-                    var addition = undefined;
-                    for (var i in ann[type].label) {
-                        var label = ann[type].label[i];
-                        var idx = label.tokens.indexOf(t);
-                        if (idx >= 0) {
-                            var rest = label.tokens.splice(idx, label.tokens.length - idx);
-                            rest.shift();
+                var sentence = []
+                var ann = 0;
+                var annotation = annotationSet[ann];
+                for (var k in tokens) {
+                    var v = tokens[k].word;
 
-                            if (rest.length) {                            
-                                addition = {name: label.name, tokens: rest};
-                            }
+                    if (v.match(/\*\w+\*/)) {
+                        v = '';
+                    }
+
+                    if (angular.isDefined(annotation)){
+                        var colors = self.getColors(annotation.name);
+
+                        if (self.selected(i) && k >= annotation.tokens[0] && k <= annotation.tokens[annotation.tokens.length-1]){
                             
-                            if (!label.tokens.length) {
-                                ann[type].label.splice(i, 1);
-                            }
+                            self.activeSentence[k].style = {
+                                'background-color': '#'+colors[0],
+                                'color': '#'+colors[1]
+                            };
+                        }
 
-                            break;
+                        if (k == annotation.tokens[0]) {
+                            v = '<span style="background-color:#'+colors[0]+'; color:#'+colors[1]+'">'+v;
+                        } 
+                        
+                        if (k == annotation.tokens[annotation.tokens.length-1]) {
+                            v = v+'</span>';
+                            annotation = annotationSet[++ann];
                         }
                     }
 
-                    if (angular.isDefined(addition)) {
-                        ann[type].label.push(addition);
-                    }
+                    sentence.push(v);
                 }
-            });
-        };
 
+                sentences.push($sce.trustAsHtml(sentence.join(' ')));
+            }
+
+            self.sentences = sentences;
+        };
 
         self.getColors = function (name) {
             for (var i in self.fes.core) {
@@ -155,6 +178,7 @@
                         }
                     }).filter(function (x) {return !angular.isUndefined(x)})
                 };
+		self.select(0);
             });
         }
     };
